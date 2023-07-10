a160f50a4bca87eef320fa14d9ba01d4e48e0605
heeren17 <miyukichan003@gmail.com>
2023-07-11 04:29:06 +0700

    Login-B

--- app/src/main/java/com/fahmidwiwpamane202102364/login/MenuActivity.java
+++ app/src/main/java/com/fahmidwiwpamane202102364/login/MenuActivity.java
@@ -2,13 +2,34 @@ package com.fahmidwiwpamane202102364.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button _tampilMahasiswaButton;
+
+    private Button _tampilMahasiswaIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        _tampilMahasiswaButton = findViewById(R.id.tampilMahasiswaButton);
+
+        _tampilMahasiswaButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
