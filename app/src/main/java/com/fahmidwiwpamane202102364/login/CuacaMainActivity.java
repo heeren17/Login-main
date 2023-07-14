e9e17f2acc9eab8dad9e606dc79edeab7bc35425
heeren17 <miyukichan003@gmail.com>
2023-07-14 19:11:57 +0700

    Login

--- app/src/main/java/com/fahmidwiwpamane202102364/login/CuacaMainActivity.java
+++ app/src/main/java/com/fahmidwiwpamane202102364/login/CuacaMainActivity.java
@@ -1,13 +1,13 @@
 package com.fahmidwiwpamane202102364.login;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
@@ -16,10 +16,10 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import cz.msebera.android.httpclient.Header;
 
 public class CuacaMainActivity extends AppCompatActivity {
-
     private RecyclerView _recyclerView2;
     private SwipeRefreshLayout _swipeRefreshLayout2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,45 +32,38 @@ public class CuacaMainActivity extends AppCompatActivity {
         initSwipeRefreshLayout();
     }
 
-    private void initSwipeRefreshLayout() {
+    private void initSwipeRefreshLayout()
+    {
         _swipeRefreshLayout2.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                initRecyclerView2();
+                initSwipeRefreshLayout();
+                _swipeRefreshLayout2.setRefreshing(false);
             }
         });
     }
 
-    private void initRecyclerView2() {
-
-        _swipeRefreshLayout2.setRefreshing(true);
-
-        String url = "https://api.openweathermap.org/data/2.5/forecast?id=1630789&appid=3ce001a40393540d4e9a027e45c7c283";
+    private void initRecyclerView2(){
+        String url = "https://api.openweathermap.org/data/2.5/forecast?id=1630789&appid=ba8c960383bb17d0b62ed0c0f78bfeaa";
         AsyncHttpClient ahc = new AsyncHttpClient();
 
         ahc.get(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-                Log.d( "*tw*", new String(responseBody));
+                //Log.d("*tw*", new String(responseBody));
                 Gson gson = new Gson();
-
                 CuacaRootModel rm = gson.fromJson(new String(responseBody), CuacaRootModel.class);
-
+                //Log.d("*tw*", rm.getListModelList().get(0).getDt_txt());
                 RecyclerView.LayoutManager lm = new LinearLayoutManager(CuacaMainActivity.this);
                 CuacaAdapter ca = new CuacaAdapter(rm);
 
                 _recyclerView2.setLayoutManager(lm);
                 _recyclerView2.setAdapter(ca);
-
-                _swipeRefreshLayout2.setRefreshing(false);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                _swipeRefreshLayout2.setRefreshing(false);
-
             }
         });
     }
